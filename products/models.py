f6065a9a020278e811481a82f663d484facfd346
Inbal <doar001@gmail.com>
2024-10-03 18:20:19 +0300

    add picture to category name

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     popularity = models.IntegerField()
+    image = models.ImageField(null=True,blank=True,default='/placeholder.png')
 
     def __str__(self):
         return f"{self.name} - {self.popularity}"
